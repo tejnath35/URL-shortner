b645ec59b4992f5c0482e285cfe64eb41138d8bf
tejnath35 <tejnathg35@gmail.com>
2026-07-22 23:35:53 +0530

    a

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./Pages/Home.jsx";
 import Dashboard from "./Pages/Dashboard.jsx";
 import ProfileDashboard from "./Pages/ProfileDashboard.jsx";
